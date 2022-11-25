6828391413a6dce994ef967a1599655b7c32f8f8
JustinFay01 <justin.fay@hope.edu>
2022-11-25 11:39:09 -0500

    Mouse-Click Working

--- C++/mouse-click.cpp
+++ C++/mouse-click.cpp
@@ -2,6 +2,7 @@
 #include <winuser.h>
 #include <stdio.h>
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
@@ -13,8 +14,17 @@ int main() {
         cout << "x = " << p.x << "y = " << p.y << endl;
        
     }
+
+    cout << "What champ you want foo?" << endl;
+    string champ;
+    getline(cin, champ);
+    cout << champ << " it is." << endl;
+    
     SetCursorPos(50,87);
     mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+
+
+
     
     return 0;
 }
