38659fa78ce3f6ecd77aaf2778998fc8b6f43e7f
JustinFay01 <JustinFay@hope.edu>
2022-11-26 17:39:54 -0500

    Almost done

--- C++/CharacterLockIn/mouse-click.cpp
+++ C++/CharacterLockIn/mouse-click.cpp
@@ -8,8 +8,37 @@ using namespace std;
 
 void lockin()
 {
-    SetCursorPos(725, 434);
+    SetCursorPos(1292, 1054);
+    //click();
 }
+
+void click(){
+     mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+}
+/**
+Character Cordinates for Main Monitor
+Estimate
+Lock in Button -> 1283 1083
+Astra -> x = 774y = 1228
+Breach -> x = 896y = 1220
+Brim -> x = 1005y = 1228
+Chamber -> x = 1114y = 1237
+Cypher -> x = 1234y = 1238
+Fade -> x = 1349y = 1240
+Jett -> x = 1453y = 1239
+Kayo -> x = 1566y = 1233
+KillJoy -> x = 1683y = 1237
+Omen -> x = 1792y = 1242
+Phoenix -> x = 782y = 1337
+Raze -> x = 886y = 1334
+Reyna -> x = 1010y = 1342
+Sage -> x = 1121y = 1347
+Skye -> x = 1233y = 1347
+Neon -> x = 1336y = 1344
+Yoru ->x = 1456y = 1346
+x = 1570y = 1347
+x = 1679y = 1344
+*/ 
 
 int main()
 {
@@ -18,8 +47,8 @@ int main()
     // sets display to be used
     HWND hWnd{0};
 
-    printf("Welcome to instant character select\nPress Q to quit or hold the corresponding number for the character you want:\n");
-    printf("1 -> Champ 1..\n2 -> Champ 2..\n");
+    printf("Welcome to instant character select\nPress Q to quit, C to print current mouse cordinates or hold the corresponding number for the character you want:\n");
+    printf("1 -> Reyna..\n2 -> KillyJoy..\n3 -> Jett..\n4 -> Chamber.\n");
 
     int running = 1;
     while (running == 1)
@@ -31,22 +60,38 @@ int main()
             // print which character is being locked in
             printf("Locking in Renya...\n");
             // set and click
-            SetCursorPos(456, 468);
-            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+            SetCursorPos(1228, 1228);
+            
 
             lockin();
         }
         else if (GetKeyState('2') & 0x8000)
         {
-            GetCursorPos(&p);
-            cout << "x = " << p.x << "y = " << p.y << endl;
+            printf("Locking in KillyJoy...\n");
+            SetCursorPos(1683,1237);
+            //mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+
+            lockin();
         }
         else if (GetKeyState('3') & 0x8000)
+        {
+            printf("Locking in Jett...\n");
+            SetCursorPos(1453, 1239);
+
+            lockin();
+        }
+        else if (GetKeyState('4') & 0x8000)
+        {
+            printf("Locking in Chamber...\n");
+            SetCursorPos(1114, 1237);
+
+            lockin();
+        }
+        else if (GetKeyState('C') & 0x8000)
         {
             GetCursorPos(&p);
             cout << "x = " << p.x << "y = " << p.y << endl;
         }
-
         // Used as quit command
         else if (GetKeyState('Q') & 0x8000)
         {
