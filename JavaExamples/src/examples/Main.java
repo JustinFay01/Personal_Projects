c1258a94a9263510c3fb4d19b407f160548c603a
Justin Fay <justin.fay@hope.edu>
2022-12-03 23:39:20 -0500

    Added Coin Problem and KnapSackMF

---
+++ JavaExamples/src/examples/Main.java
@@ -0,0 +1,69 @@
+package examples;
+
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		loadSack();
+
+		MFKnapsack(sack.length - 1, capacity - 1);
+
+		print2D(sack);
+
+		for (int i = 1; i < weights.length; i++) {
+			for (int j = 1; j < capacity; j++) {
+				MFKnapsack(i, j);
+			}
+		}
+		System.out.println();
+		print2D(sack);
+	}
+
+	public static void print2D(int arr[][]) {
+		// Loop through all rows
+		for (int[] row : arr)
+
+			// converting each row as string
+			// and then printing in a separate line
+			System.out.println(Arrays.toString(row));
+	}
+
+	private static int[] weights = { 0, 3, 2, 1, 4, 5 };
+	private static int[] values = { 0, 25, 20, 15, 40, 50 };
+	public static int capacity = 7;
+	public static int[][] sack = new int[values.length][capacity];
+
+	public static void loadSack() {
+		for (int i = 0; i < sack.length; i++) {
+			for (int j = 0; j < sack[0].length; j++) {
+				if (i == 0 || j == 0)
+					sack[i][j] = 0;
+				else
+					sack[i][j] = -1;
+			}
+		}
+	}
+
+	public static int MFKnapsack(int i, int j) {
+		int value = 0;
+		if (sack[i][j] < 0) {
+			if (j < weights[i]) {
+
+				value = MFKnapsack(i - 1, j);
+			} else {
+				if (MFKnapsack(i - 1, j) > values[i] + MFKnapsack(i - 1, j - weights[i])) {
+
+					value = MFKnapsack(i - 1, j);
+				} else {
+					value = values[i] + MFKnapsack(i - 1, j - weights[i]);
+				}
+
+			}
+			sack[i][j] = value;
+		}
+		return sack[i][j];
+	}
+
+}
---
+++ JavaExamples/src/tests/algorithimsTest.java
@@ -0,0 +1,46 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+import examples.Algorithims;
+
+class algorithimsTest {
+
+	@Test
+	void coinRowTest() {
+		Algorithims algo = new Algorithims();
+		
+		int[] C = new int[8];
+		C[0] = -1;
+		C[1] = 5;
+		C[2] = 1;
+		C[3] = 2;
+		C[4] = 10;
+		C[5] = 6;
+		C[6] = 2;
+		
+		
+		int[] F = algo.coinRowProblem(C);
+		System.out.println("Original: "+ Arrays.toString(C));
+		System.out.println("  Solved: "+ Arrays.toString(F));
+		//assertTrue(Arrays.equals(F, F));
+		
+		C[0] = -1;
+		C[1] = 6;
+		C[2] = 2;
+		C[3] = 3;
+		C[4] = 9;
+		C[5] = 11;
+		C[6] = 8;
+		C[7] = 5;
+		
+		F = algo.coinRowProblem(C);
+		System.out.println("Original: "+ Arrays.toString(C));
+		System.out.println("  Solved: "+ Arrays.toString(F));
+	}
+
+}
