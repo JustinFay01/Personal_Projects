e2448fdb4c3f129ab05f248754f6df917790588b
Justin Fay <justin.fay@hope.edu>
2022-12-04 21:11:55 -0500

    CSV Works

--- Python/scrape.py
+++ Python/scrape.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 from csv import writer
 
-url= "https://www.linkedin.com/jobs/search/?currentJobId=3380094907&keywords=software%20engineer%20internship"
+url= "https://www.linkedin.com/jobs/search/?currentJobId=3362033747&keywords=software%20engineer%20internship"
 page = requests.get(url)
 # print(page) 
  # will display HTTP reponse codes 200 - 299 is sucessful
@@ -16,13 +16,14 @@ contents = soup.find_all('div', class_= "base-search-card__info")
 
 with open('softwareinternship.csv', 'w', encoding='utf8', newline='') as f:
     thewriter = writer(f)
-    header = ['Title', 'Link']
-    # thewriter.writerow(header)
+    header = ['Title', 'Company', 'Link']
+    thewriter.writerow(header)
 
     for content in contents:
         title = content.find('h3', class_="base-search-card__title").text.replace('\n            \n        ', '')
-        info = content.find('div', class_="base-search-card__info")
-        link = content.find('a', href_="https:")
-        info = [title, info,link]
+        info = content.find('h4', class_="base-search-card__subtitle").text.replace('\n\n            ','')
+        for link in content.find_all('a'):
+            links = link.get('href')
+        info = [title,info,links]
         print(info)
-        #thewriter.writerow(info)
+        thewriter.writerow(info)
