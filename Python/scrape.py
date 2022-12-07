5bb8b556694266fc8d23e403fba6a1072b9dbf0a
Justin Fay <justin.fay@hope.edu>
2022-12-07 00:19:40 -0500

    Just need company name then can work on app

--- Python/scrape.py
+++ Python/scrape.py
@@ -2,28 +2,32 @@ from bs4 import BeautifulSoup
 import requests
 from csv import writer
 
-url= "https://www.linkedin.com/jobs/search/?currentJobId=3362033747&keywords=software%20engineer%20internship"
+url= "https://www.linkedin.com/jobs/search/?currentJobId=3289565209&f_JT=I&geoId=103112676&keywords=software%20engineer%20internship&location=Chicago%2C%20Illinois%2C%20United%20States&refresh=true"
 page = requests.get(url)
 # print(page) 
  # will display HTTP reponse codes 200 - 299 is sucessful
 
 soup = BeautifulSoup(page.content, 'html.parser')
-# print(soup)
+# print(soup.prettify)
 
-
-contents = soup.find_all('div', class_= "base-search-card__info")
-# print(contents)
+contents = soup.find_all('a', class_= "base-card__full-link")
+#print(contents)
 
 with open('softwareinternship.csv', 'w', encoding='utf8', newline='') as f:
     thewriter = writer(f)
-    header = ['Title', 'Company', 'Link']
+    header = ['Title','Link', 'Company']
     thewriter.writerow(header)
 
+
+    
     for content in contents:
-        title = content.find('h3', class_="base-search-card__title").text.replace('\n            \n        ', '')
-        info = content.find('h4', class_="base-search-card__subtitle").text.replace('\n\n            ','')
-        for link in content.find_all('a'):
-            links = link.get('href')
-        info = [title,info,links]
-        print(info)
+        title = content.find('span', class_="sr-only").text.replace('\n            \n        \n        ', '').replace('\n      \n      \n          ','')
+        links = content.get('href')
+        company = soup.find_all_next('h4', class_= 'base-search-card__subtitle')
+        info = [title, company, links]
         thewriter.writerow(info)
+
+
+    
+
+
