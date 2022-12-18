58595b28f8ea17335b330b814559ba11af2d07a6
Justin Fay <justin.fay@hope.edu>
2022-12-17 22:45:25 -0500

    Added Knap Sack by hand in Main and formatted printing better

--- JavaExamples/src/examples/misc/KnapSack.java
+++ JavaExamples/src/examples/misc/KnapSack.java
@@ -4,24 +4,35 @@ import java.util.Arrays;
 
 public class KnapSack {
 
+	// USED FOR MF KNAPSACK
+	private static int[] weights = { 0, 1, 6, 2, 2, 2, 5, 4, 1 };
+	private static int[] values = { 0, 1, 4, 3, 6, 1, 8, 10, 2 };
+	public static int capacity = 11;
+	public static int[][] sack = new int[values.length][capacity];
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
 		loadSack();
-		MFKnapsack(weights.length-1, capacity-1);
+		MFKnapsack(weights.length - 1, capacity-1);
 		print2D(sack);
 		
-
-	
+		
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		//By Hand Example
+		loadSack();
+		for(int i = 1; i < weights.length; i++) {
+			for(int j = 1; j < capacity; j++) {
+				MFKnapsack(i,j);
+			}
+		}
+		print2D(sack);
 	}
-
-	
 
 	public static int MFKnapsack(int i, int j) {
 		int value = 0;
 		if (sack[i][j] < 0) {
 			if (j < weights[i]) {
-
 				value = MFKnapsack(i - 1, j);
 			} else {
 				if (MFKnapsack(i - 1, j) > values[i] + MFKnapsack(i - 1, j - weights[i])) {
@@ -37,31 +48,39 @@ public class KnapSack {
 		return sack[i][j];
 	}
 	
-	// USED FOR MF KNAPSACK
-		private static int[] weights = { 0, 1, 6, 2, 2, 2, 5, 4, 1 };
-		private static int[] values = {  0, 1, 4, 3, 6, 1, 8, 10, 2 };
-		public static int capacity = 11;
-		public static int[][] sack = new int[values.length][capacity];
-		// USED FOR MFKNAPSACK
-
-		public static void loadSack() {
-			for (int i = 0; i < sack.length; i++) {
-				for (int j = 0; j < sack[0].length; j++) {
-					if (i == 0 || j == 0)
-						sack[i][j] = 0;
-					else
-						sack[i][j] = -1;
-				}
+	public static void loadSack() {
+		for (int i = 0; i < sack.length; i++) {
+			for (int j = 0; j < sack[0].length; j++) {
+				if (i == 0 || j == 0)
+					sack[i][j] = 0;
+				else
+					sack[i][j] = -1;
 			}
 		}
-		
-		public static void print2D(int arr[][]) {
-			// Loop through all rows
-			for (int[] row : arr)
+	}
 
-				// converting each row as string
-				// and then printing in a separate line
-				System.out.println(Arrays.toString(row));
+	public static void print2D(int arr[][]) {
+		// Loop through all rows
+		
+		System.out.println("Weights: " + Arrays.toString(weights));
+		System.out.println("Values: " + Arrays.toString(values) + '\n');
+		
+		for (int[] row : arr) {
+			// converting each row as string
+			// and then printing in a separate line
+			//System.out.println(Arrays.toString(row));
+			for(int i = 0; i < row.length; i++) {
+				if(i == 0)
+					System.out.print("[" +row[i] + ", ");
+				else if(i + 1 != row.length) {
+					System.out.print("\t" +row[i] + ", ");
+				}
+				else {
+					System.out.print("\t" + row[i] + "]");
+				}
+			}
+			System.out.println();
 		}
+	}
 
 }
