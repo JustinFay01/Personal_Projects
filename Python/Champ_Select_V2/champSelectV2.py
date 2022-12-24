93113f0fd0ebba749f7d87039e4f517c22fd932d
Justin Fay <Justin.Fay@hope.edu>
2022-12-24 15:39:19 -0500

    Got keyboard input working

--- Python/Champ_Select_V2/champSelectV2.py
+++ Python/Champ_Select_V2/champSelectV2.py
@@ -1,28 +1,17 @@
 import pyautogui
-from pynput import keyboard
+import keyboard  # using module keyboard
+
 
 print(pyautogui.size())
 
-def on_press(key):
-    try:
-        print('alphanumeric key {0} pressed'.format(
-            key.char))
-    except AttributeError:
-        print('special key {0} pressed'.format(
-            key))
 
-def on_release(key):
-    print('{0} released'.format(
-        key))
-    if key == keyboard.Key.esc:
-        # Stop listener
-        return False
-
-# Collect events until released
-with keyboard.Listener(
-        on_press=on_press,
-        on_release=on_release) as listener:
-    listener.join()
+while True:  # making a loop
+    try:  # used try so that if user pressed other than the given key error will not be shown
+        if keyboard.is_pressed('q'):  # if key 'q' is pressed 
+            print('You Pressed A Key!')
+            break  # finishing the loop
+    except:
+        break  # if user pressed a key other than the given key the loop will break
 
 
 
