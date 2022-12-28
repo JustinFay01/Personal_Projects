a0495a6faba4980ce5f3e4ce95d3b45b3a563f2c
Justin Fay <Justin.Fay@hope.edu>
2022-12-27 23:08:48 -0500

    Moved Cube location

--- CExamples/cube.c
+++ CExamples/cube.c
@@ -10,7 +10,7 @@ int width = 160, height = 44;
 float zBuffer[160 * 44];
 char buffer[160 * 44];
 int backgroundASCIICode = ' ';
-int distanceFromCam = 100;
+int distanceFromCam = 75;
 float horizontalOffset;
 float K1 = 40;
 
@@ -61,17 +61,17 @@ int main() {
     memset(buffer, backgroundASCIICode, width * height);
     memset(zBuffer, 0, width * height * 4);
     cubeWidth = 20;
-    horizontalOffset = -2 * cubeWidth;
+    horizontalOffset = -6 * cubeWidth;
     // first cube
     for (float cubeX = -cubeWidth; cubeX < cubeWidth; cubeX += incrementSpeed) {
       for (float cubeY = -cubeWidth; cubeY < cubeWidth;
            cubeY += incrementSpeed) {
         calculateForSurface(cubeX, cubeY, -cubeWidth, '@');
-       // calculateForSurface(cubeWidth, cubeY, cubeX, '$');
-        //calculateForSurface(-cubeWidth, cubeY, -cubeX, '~');
-       // calculateForSurface(-cubeX, cubeY, cubeWidth, '#');
-       // calculateForSurface(cubeX, -cubeWidth, -cubeY, ';');
-       // calculateForSurface(cubeX, cubeWidth, cubeY, '+');
+        calculateForSurface(cubeWidth, cubeY, cubeX, '$');
+        calculateForSurface(-cubeWidth, cubeY, -cubeX, '~');
+        calculateForSurface(-cubeX, cubeY, cubeWidth, '#');
+        calculateForSurface(cubeX, -cubeWidth, -cubeY, ';');
+        calculateForSurface(cubeX, cubeWidth, cubeY, '+');
       }
     }
     printf("\x1b[H");
