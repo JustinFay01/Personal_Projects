b6214cb3daee0d7815c012a6f91b21ad3c0890d1
Justin Fay <Justin.Fay@hope.edu>
2023-01-02 15:25:50 -0500

    wordPattern leet Code: Given a pattern and a string s, find if s follows the same pattern.
    Here follow means a full match, such that there is a bijection between a
    letter in pattern and a non-empty word in s.

--- JavaExamples/src/examples/StringExamples.java
+++ JavaExamples/src/examples/StringExamples.java
@@ -1,9 +1,43 @@
 package examples;
 
-import java.util.Arrays;
-import java.util.Set;
+import java.util.*;
 
 public class StringExamples {
+
+	/*
+	 * Given a pattern and a string s, find if s follows the same pattern.
+	 * 
+	 * Here follow means a full match, such that there is a bijection between a
+	 * letter in pattern and a non-empty word in s.
+	 */
+	public boolean wordPattern(String pattern, String s) {
+		HashMap<Character, String> map_char = new HashMap<>();
+		HashMap<String, Character> map_word = new HashMap<>();
+		String[] words = s.split(" ");
+
+		if (words.length != pattern.length())
+			return false;
+
+		for (int i = 0; i < words.length; i++) {
+			char c = pattern.charAt(i);
+			String w = words[i];
+			if (!map_char.containsKey(c)) {
+				if (map_word.containsKey(w)) {
+					return false;
+				} else {
+					map_char.put(c, w);
+					map_word.put(w, c);
+				}
+
+			} else {
+				String mapped_word = map_char.get(c);
+				if (!mapped_word.equals(w))
+					return false;
+			}
+		}
+
+		return true;
+	}
 
 	/*
 	 * Given two strings s and t, return true if s is a subsequence of t, or false
